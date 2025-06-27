2f30553599ae335700777800bbd940ab68720c16
Murad Najafli <muradnecefli05@gmail.com>
2025-06-27 16:46:39 +0400

    Implement SpinWheel

--- app/src/main/java/com/muradnajafli/composelab/MainActivity.kt
+++ app/src/main/java/com/muradnajafli/composelab/MainActivity.kt
@@ -6,7 +6,6 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
 import com.muradnajafli.composelab.spin_wheel.SpinWheelExampleUsage
